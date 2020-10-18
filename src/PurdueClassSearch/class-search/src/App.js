017c1833723fcb78fdffe1a82a190507e6b012bb
Karthik Menon <ksmenon10@gmail.com>
2020-10-18 08:22:05 -0500

    Start front end code

---
+++ src/PurdueClassSearch/class-search/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Col, Form, Button, Dropdown } from "react-bootstrap";
+import "./App.css";
+
+
+function App() {
+    const [major, setMajor] = useState();
+    const [minor, setMinor] = useState();
+
+    const handleSubmit = (e) => {
+        const form = e.currentTarget;
+        e.preventDefault();
+        console.log(major);
+    }
+
+    return (
+        <div className="whole">
+            <div className="top-block">
+                <h1 className="message">Welcome</h1>
+            </div>
+            <div className="bottom-block">
+                <Form>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="validationCustom01">
+                            <Form.Label>Major</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Major"
+                                onChange={(e) => setMajor(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="validationCustom01">
+                            <Form.Label>Minor</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Minor"
+                                onChange={(e) => setMinor(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Form.Row>
+                    <Button type="submit">Get results</Button>
+                </Form>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
